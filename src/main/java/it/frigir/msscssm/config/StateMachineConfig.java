4bf788d63465d251247208c7b74383876d12ad26
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-03 15:11:30 +0200

    state machine config and test

---
+++ src/main/java/it/frigir/msscssm/config/StateMachineConfig.java
@@ -0,0 +1,37 @@
+package it.frigir.msscssm.config;
+
+import it.frigir.msscssm.domain.PaymentEvent;
+import it.frigir.msscssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+
+import java.util.EnumSet;
+
+@Slf4j
+@EnableStateMachineFactory
+@Configuration
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW) // initial state of machine
+                .states(EnumSet.allOf(PaymentState.class)) // list of states
+                .end(PaymentState.AUTH) // set end state
+                .end(PaymentState.AUTH_ERR) // set end state
+                .end(PaymentState.PRE_AUTH_ERR); // set end state
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
+        transitions.withExternal()
+                .source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
+                .and()
+                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
+                .and()
+                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERR).event(PaymentEvent.PRE_AUTH_DECLINED);
+    }
+}
---
+++ src/test/java/it/frigir/msscssm/config/StateMachineConfigTest.java
@@ -0,0 +1,35 @@
+package it.frigir.msscssm.config;
+
+import it.frigir.msscssm.domain.PaymentEvent;
+import it.frigir.msscssm.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        // create new state machine
+        StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+        sm.start();
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+
+        System.out.println(sm.getState().toString());
+    }
+}
