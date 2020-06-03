7650b08df91143a7830471bc53b07bea8df83701
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-03 14:51:16 +0200

    state and event

---
+++ src/main/java/it/frigir/msscssm/domain/PaymentEvent.java
@@ -0,0 +1,5 @@
+package it.frigir.msscssm.domain;
+
+public enum PaymentEvent {
+    PRE_AUTHORIZE, PRE_AUTH_APPROVED, PRE_AUTH_DECLINED, AUTHORIZED, AUTH_APPROVED, AUTH_DECLINED
+}
---
+++ src/main/java/it/frigir/msscssm/domain/PaymentState.java
@@ -0,0 +1,5 @@
+package it.frigir.msscssm.domain;
+
+public enum PaymentState {
+    NEW, PRE_AUTH, PRE_AUTH_ERR, AUTH, AUTH_ERR
+}
