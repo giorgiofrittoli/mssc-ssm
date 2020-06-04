17222a77e0258aaefaa49296def19e1b72183fc3
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-04 10:10:49 +0200

    create new statemachine with payment state

--- src/test/java/it/frigir/msscssm/config/StateMachineConfigTest.java
+++ src/test/java/it/frigir/msscssm/config/StateMachineConfigTest.java
@@ -31,5 +31,10 @@ class StateMachineConfigTest {
         sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
 
         System.out.println(sm.getState().toString());
+
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_DECLINED);
+
+        System.out.println(sm.getState().toString());
     }
 }
