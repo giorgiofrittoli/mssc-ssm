313d979ac0bff27ce407213ef314567eb74468af
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-04 10:53:42 +0200

    service test

---
+++ src/test/java/it/frigir/msscssm/services/PaymentServiceImplTest.java
@@ -0,0 +1,39 @@
+package it.frigir.msscssm.services;
+
+import it.frigir.msscssm.domain.Payment;
+import it.frigir.msscssm.repository.PaymentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+
+@SpringBootTest
+class PaymentServiceImplTest {
+
+    @Autowired
+    PaymentService paymentService;
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    Payment payment;
+
+    @BeforeEach
+    void setUp() {
+        payment = Payment.builder().amount(new BigDecimal("12.99")).build();
+    }
+
+    @Transactional
+    @Test
+    void preAuth() {
+        Payment savedPayment = paymentService.newPayment(payment);
+        paymentService.preAuth(savedPayment.getId());
+
+        Payment preAuthPayment = paymentRepository.getOne(savedPayment.getId());
+
+        System.out.println(preAuthPayment);
+    }
+}
